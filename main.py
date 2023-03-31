52b4df9bebb602d74934b13e868220b248e3e503
OJ <omj82004@gmail.com>
2023-03-30 23:47:22 -0400

    random msg

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env pybricks-micropython
 
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor
-# from pybricks.ev3devices import GyroSensor
-# from pybricks.ev3devices import MediumMotor
-from pybricks.tools import wait
+from pybricks.ev3devices import Motor
 from pybricks.parameters import Port
 from pybricks.robotics import DriveBase
 
 #Take all values
 turn_degs=180
 distn_cm=100
-v_dps=-500 # Velocity in degs per second
+v_dps=500 # Velocity in degs per second
 
 # Callibrated distn to make bot actual go entered distance
 distn=distn_cm*14
@@ -20,12 +17,8 @@ distn=distn_cm*14
 ev3 = EV3Brick()
 
 # Initialize the motors.
-lift_motor = Motor(Port.A)
 left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
-proxi_sensor = UltrasonicSensor(Port.S1)
-gyro_sensor = GyroSensor(Port.S4)
-
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=43.8, axle_track=160)
@@ -78,7 +71,7 @@ def turn():
 def lift():
     lift_motor.run(speed=-500)
     wait(500)
-    lift_motor.stop()
+    lift_motor.brake()
 
 # lift()
 
@@ -88,4 +81,7 @@ def proxi():
         right_motor.run(speed=500)
         if proxi_sensor.distance() < 300:
             break
+    left_motor.brake()
+    right_motor.brake() 
+
 proxi()
